2fcfb1ac49d42759d33732762b3b8fb924c806cc
Laura <lauraquercc@gmail.com>
2023-11-11 20:05:18 +0100

    generic function to create outfit addapted depending on patterns

--- main.py
+++ main.py
@@ -15,45 +15,37 @@ def print_log(message):
     print(f'{message}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def cluster_outfits(data):
-    # Supongamos que tienes un DataFrame llamado 'dataset' con las columnas 'cod_outfit', 'producto', 'color', 'talla', y otras columnas.
-    # Reemplaza esto con tu propio DataFrame
-    dataset = pd.DataFrame(data)
-
-    # Seleccionar características relevantes (en este caso, solo 'color' y 'talla')
-    X = dataset[['des_color_specification_esp', 'des_fabric']]
-
-    # Codificar variables categóricas a valores numéricos
-    X_encoded = pd.get_dummies(X)
-
-    # Escalar las características para que todas tengan la misma importancia
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X_encoded)
-
-    # Aplicar el algoritmo DBSCAN
-    dbscan = DBSCAN(eps=0.5, min_samples=2)  # Puedes ajustar los parámetros según tus necesidades
-    dataset['cluster'] = dbscan.fit_predict(X_scaled)
-
-    # Visualizar los clusters
-    sns.scatterplot(x=X_scaled[:, 0], y=X_scaled[:, 1], hue=dataset['cluster'], palette='viridis', s=100)
-    plt.title('Clusters de prendas compatibles')
-    plt.xlabel('Color (escala)')
-    plt.ylabel('Fabric (escala)')
-    plt.legend(title='Cluster')
-    plt.show()
-
-
-def create_outfit(tops_data, bottoms_data, footwear_data):
+def create_outfit(tops_data, bottoms_data, footwear_data, patterns):
+    # generate random top and for every pattern save the specific characteristic of top
     top = tops_data.sample()
-    bottom = bottoms_data.sample()
-    footwear = footwear_data.sample()
-    """color = top['des_color_specification_esp'].values[0]
-    print("color: ", color)
-    if len(bottoms_data[bottoms_data['des_color_specification_esp'] == color]) > 0:
-        bottom = bottoms_data[bottoms_data['des_color_specification_esp'] == color].sample()
-    else:
+    top_pattern = []
+    sex = top['des_sex'].values[0]
+    for pattern in patterns:
+        top_pattern.append(top[pattern[0]].values[0])
+
+    # find a valid bottom
+    validBottom = False
+    while not validBottom:
         bottom = bottoms_data.sample()
-    footwear = footwear_data.sample()"""
+        for idx, pattern in enumerate(patterns):
+            if bottom[pattern[0]].values[0] == top_pattern[idx]\
+                    and (bottom['des_sex'].values[0] == sex or bottom['des_sex'].values[0] == 'Unisex'):
+                validBottom = True
+            else:
+                validBottom = False
+
+    # find a valid footwear
+    validFootwear = False
+    while not validFootwear:
+        footwear = footwear_data.sample()
+        for idx, pattern in enumerate(patterns):
+            if footwear[pattern[0]].values[0] == top_pattern[idx]\
+                    and (footwear['des_sex'].values[0] == sex or footwear['des_sex'].values[0] == 'Unisex'):
+                validFootwear = True
+            else:
+                validFootwear = False
+
+
     return [top['cod_modelo_color'], bottom['cod_modelo_color'], footwear['cod_modelo_color']]
 
 
@@ -76,6 +68,18 @@ def add_outfit(new_products_list):
     custom_outfit_data.to_csv(path, mode='w', index=False)
 
 
+def csv_to_dic(pattern_data, idx_col, heuristic):
+    df = pd.read_csv(pattern_data, index_col=idx_col)
+
+    relaciones_dict = {}
+    for color in df.index:
+        fila = df.loc[color]
+        colores_relacionados = fila[fila >= heuristic].index.tolist()
+        relaciones_dict[color] = colores_relacionados
+
+    return relaciones_dict
+
+
 def main():
     outfit_data = pd.read_csv('datathon/dataset/outfit_data.csv')
     product_data = pd.read_csv('datathon/dataset/product_data.csv')
@@ -85,23 +89,33 @@ def main():
     joined_data = joined_data.sort_values(by='cod_outfit')
     joined_data.to_csv('datathon/dataset/joined_data.csv', index=False)"""
 
+    # type of elements of one outfi
     tops_data = product_data[product_data['des_product_category'] == 'Tops']
     bottoms_data = product_data[product_data['des_product_category'] == 'Bottoms']
     footwear_data = product_data[product_data['des_product_family'] == 'Footwear']
 
+    # declaration of patterns
+    patterns = []
+    color_matching = csv_to_dic("datathon/patterns/color_matching.csv", "des_agrup_color_eng", 300)
+    patterns.append(["des_agrup_color_eng",color_matching])
+
+    # outfits creation
     for i in range(0,9):
-        list_outfit_products = create_outfit(tops_data, bottoms_data, footwear_data)
+        list_outfit_products = create_outfit(tops_data, bottoms_data, footwear_data, patterns)
         add_outfit(list_outfit_products)
 
     custom_outfit_data = pd.read_csv('datathon/dataset/custom_outfit_data.csv')
     custom_joined_data = pd.merge(custom_outfit_data, product_data, on='cod_modelo_color', how='inner')
     custom_joined_data.to_csv('datathon/dataset/custom_joined_data.csv', index=False)
 
-    patternDiscovery = PatternDiscovery()
-    patternDiscovery.color_matching()
+
+    #patternDiscovery = PatternDiscovery()
+    #patternDiscovery.color_matching()
 
     viewer = OutfitViewer()
     viewer.run()
+
+
 
 
 if __name__ == '__main__':
