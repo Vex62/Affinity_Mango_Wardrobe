ebd7c1d2cfa67227f0b0ff7e0bd6eb3fcaaebc3b
Laura <lauraquercc@gmail.com>
2023-11-12 10:12:20 +0100

    creating outfits with complements

--- PatternDiscovery.py
+++ PatternDiscovery.py
@@ -1,10 +1,7 @@
-import kivy
-import pandas as pd
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 import pandas as pd
-from kivy.uix.image import Image
-from kivy.uix.button import Button
+import networkx as nx
+from matplotlib import pyplot as plt
 
 
 class PatternDiscovery(App):
@@ -25,3 +22,34 @@ class PatternDiscovery(App):
             result_matrix.loc[outfit_colors, outfit_colors] += 1
         # Mostrar la tabla resultante
         result_matrix.to_csv("datathon/patterns/color_matching.csv")
+
+    def feature_matching(self, feature_name, data):
+        # Crear un DataFrame
+        df = pd.DataFrame(data)
+
+        # Crear una matriz de co-ocurrencia
+        co_occurrence_matrix = pd.crosstab(index=df[feature_name], columns=df['cod_outfit'])
+
+        # Inicializar una matriz de resultados con ceros
+        result_matrix = pd.DataFrame(0, index=co_occurrence_matrix.index, columns=co_occurrence_matrix.index)
+
+        # Iterar sobre los outfits
+        for outfit in co_occurrence_matrix.columns:
+            # Obtener los tipos de productos para el outfit actual
+            outfit_types = co_occurrence_matrix[co_occurrence_matrix[outfit] > 0].index
+
+            # Comparar los tipos de productos entre sÃ­
+            for i in range(len(outfit_types)):
+                for j in range(i + 1, len(outfit_types)):
+                    type1, type2 = outfit_types[i], outfit_types[j]
+
+                    # Contabilizar la coincidencia solo si son diferentes
+                    isColor = False
+                    if feature_name == "des_agrup_color_eng":
+                        isColor = True
+                    if type1 != type2 or isColor:
+                        result_matrix.at[type1, type2] += 1
+                        result_matrix.at[type2, type1] += 1
+
+        # Guardar la matriz en un archivo CSV
+        result_matrix.to_csv(f"datathon/patterns/{feature_name}_matching.csv")
--- main.py
+++ main.py
@@ -15,30 +15,68 @@ def print_log(message):
     print(f'{message}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def create_outfit(tops_data, products_data_list, patterns):
+def create_outfit(tops_data, products_data_list, patterns, joined_data, complements_data):
     # generate random top and for every pattern save the specific characteristic of top
-    top = tops_data.sample()
-    top_pattern = []
-    sex = top['des_sex'].values[0]
-    for pattern in patterns:
-        top_pattern.append(top[pattern[0]].values[0])
+    newOutfit = False
+    products = []
+    types = []
+    while not newOutfit:
+        top = tops_data.sample()
+        top_pattern = []
+        sex = top['des_sex'].values[0]
+        for pattern in patterns:
+            top_pattern.append(top[pattern[0]].values[0])
 
-    # append products
-    products = [top['cod_modelo_color']]
+        # append valid products
+        products = [top['cod_modelo_color']]
+        types = [top['des_product_type'].values[0]]
 
-    # find a matching products
-    for product_data in products_data_list:
-        validProduct = False
-        product = product_data.sample()
-        while not validProduct:
+        # find a matching products matching top characteristics
+        for product_data in products_data_list:
+            validProduct = False
             product = product_data.sample()
-            for idx, pattern in enumerate(patterns):
-                if product[pattern[0]].values[0] == top_pattern[idx]\
-                        and (product['des_sex'].values[0] == sex or product['des_sex'].values[0] == 'Unisex'):
-                    validProduct = True
-                else:
-                    validProduct = False
-        products.append(product['cod_modelo_color'])
+            while not validProduct:
+                product = product_data.sample()
+                for idx, pattern in enumerate(patterns):
+                    if product[pattern[0]].values[0] in pattern[1][top_pattern[idx]] \
+                            and (product['des_sex'].values[0] == sex or product['des_sex'].values[0] == 'Unisex'):
+                        validProduct = True
+                    else:
+                        validProduct = False
+            products.append(product['cod_modelo_color'])
+            types.append(product['des_product_type'].values[0])
+        if isNewOutfit(joined_data, products):
+            newOutfit = True
+
+    # when created a new basic outfit, adding matching complements
+    complements_df = [
+        complements_data[(complements_data['des_product_family'] == 'Jewellery')],
+        complements_data[(complements_data['des_product_family'] == 'Bags')],
+        complements_data[(complements_data['des_product_family'] == 'Hats, scarves and gloves')],
+        complements_data[(complements_data['des_product_family'] == 'Belts and Ties')],
+        complements_data[(complements_data['des_product_aggregated_family'] == 'Outwear')]]
+
+    for complement_data in complements_df:
+        validComplement = False
+        if not complement_data.empty:
+            product = complement_data.sample()
+            tries = 0
+            while not validComplement:
+                product = complement_data.sample()
+                tries = tries + 1
+                for t in types:
+                    if (product[patterns[1][0]].values[0] in patterns[1][1][str(t)]
+                        and (product['des_sex'].values[0] == sex or product['des_sex'].values[0] == 'Unisex'))\
+                            or tries >= 8:
+                        validComplement = True
+                        break
+                    else:
+                        validComplement = False
+                        break
+                if tries >= 8:
+                    break
+            if tries < 8:
+                products.append(product['cod_modelo_color'])
 
     return products
 
@@ -48,7 +86,7 @@ def add_outfit(new_products_list):
     custom_outfit_data = pd.read_csv(path)
 
     if custom_outfit_data.shape[0] > 1:
-        print("filas: "+str(custom_outfit_data.shape[0]))
+        print("filas: " + str(custom_outfit_data.shape[0]))
         nuevo_cod_outfit = custom_outfit_data['cod_outfit'].max() + 1
     else:
         nuevo_cod_outfit = 1
@@ -74,47 +112,66 @@ def csv_to_dic(pattern_data, idx_col, heuristic):
     return relaciones_dict
 
 
+def isNewOutfit(data, lista_cod_modelo_color):
+    # Agrupar por cod_outfit y verificar las combinaciones prohibidas
+    grouped_df = data.groupby('cod_outfit')['cod_modelo_color'].apply(set).reset_index()
+
+    # Verificar si hay combinaciones prohibidas en cada cod_outfit
+    repeated = grouped_df['cod_modelo_color'].apply(lambda x: any(set(comb) <= x for comb in lista_cod_modelo_color))
+    if len(repeated) > 0:
+        return True
+    else:
+        return False
+
+
 def main():
     outfit_data = pd.read_csv('datathon/dataset/outfit_data.csv')
     product_data = pd.read_csv('datathon/dataset/product_data.csv')
     joined_data = pd.read_csv('datathon/dataset/joined_data.csv')
 
-    """joined_data = pd.merge(outfit_data, product_data, on='cod_modelo_color', how='inner')
-    joined_data = joined_data.sort_values(by='cod_outfit')
-    joined_data.to_csv('datathon/dataset/joined_data.csv', index=False)"""
+    """complements_data = joined_data[
+        ((joined_data['des_product_aggregated_family'] == 'Accessories') |
+         (joined_data['des_product_aggregated_family'] == 'Outwear')) &
+        (joined_data['des_product_family'] != 'Footwear')
+        ]
+    complements_data.to_csv('datathon/dataset/complements_data.csv', index=False)"""
+    complements_data = pd.read_csv('datathon/dataset/complements_data.csv')
 
     # type of elements of one outfi
     products = []
+    # Essential products: TOP, BOTTOM, FOOTWEAR
     tops_data = product_data[product_data['des_product_category'] == 'Tops']
     bottoms_data = product_data[product_data['des_product_category'] == 'Bottoms']
     products.append(bottoms_data)
     footwear_data = product_data[product_data['des_product_family'] == 'Footwear']
     products.append(footwear_data)
-    jewellery_data = product_data[product_data['des_product_family'] == 'Jewellery']
-    products.append(jewellery_data)
+    # Additional products: OUTWEAR, JEWELLERY, BAGS, (HATS, SCRAVES AND GLOVES), (BELTS AND TIES)
+    #jewellery_data = product_data[product_data['des_product_family'] == 'Jewellery']
+    #products.append(jewellery_data)
 
     # declaration of patterns
+    # patternDiscovery = PatternDiscovery()
+    # patternDiscovery.feature_matching('des_product_type', joined_data)
+
     patterns = []
     color_matching = csv_to_dic("datathon/patterns/color_matching.csv", "des_agrup_color_eng", 300)
-    patterns.append(["des_agrup_color_eng",color_matching])
+    patterns.append(["des_agrup_color_eng", color_matching])
+    product_types = csv_to_dic("datathon/patterns/des_product_type_matching.csv", "des_product_type", 10)
+    patterns.append(["des_product_type", product_types])
 
     # outfits creation
-    for i in range(0,9):
-        list_outfit_products = create_outfit(tops_data, products, patterns)
+    for i in range(0, 9):
+        list_outfit_products = create_outfit(tops_data, products, patterns, joined_data, complements_data)
         add_outfit(list_outfit_products)
 
     custom_outfit_data = pd.read_csv('datathon/dataset/custom_outfit_data.csv')
     custom_joined_data = pd.merge(custom_outfit_data, product_data, on='cod_modelo_color', how='inner')
     custom_joined_data.to_csv('datathon/dataset/custom_joined_data.csv', index=False)
 
-
-    #patternDiscovery = PatternDiscovery()
-    #patternDiscovery.color_matching()
-
+    # view output
+    print("Initiating viewer...")
     viewer = OutfitViewer()
     viewer.run()
-
-
 
 
 if __name__ == '__main__':
